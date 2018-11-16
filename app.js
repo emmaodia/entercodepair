4c84d1f67de79bc86295d6000852b05af936793d
emmaodia <odia.emma@gmail.com>
2018-11-16 03:24:29 +0100

    Created routes dir

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+//routes
+const main = require('./routes/main')
+
 // Log requests to the console
-app.use(logger('dev'));
+app.use(morgan('dev'));;
 
 //Parse incoming data requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-
-//default routing
-app.get('*', (req, res) => res.status(200).send({
-  message: "Working... still."
-}));
 
 module.exports = app;
