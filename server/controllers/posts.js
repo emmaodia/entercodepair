587d30c1172756f4e848042a75db2f6cbbb04b3a
emmaodia <odia.emma@gmail.com>
2018-11-17 13:15:22 +0100

    Set up controllers dir

---
+++ server/controllers/posts.js
@@ -0,0 +1,13 @@
+const Post = require("../models").Post;
+
+module.exports = {
+  create(req, res) {
+    return Post
+    .create({
+      title: req.body.title,
+      body: req.body.body
+    })
+    .then(post => res.status(201).send(post))
+    .catch(error => res.status(400).send(error));
+  },
+};
